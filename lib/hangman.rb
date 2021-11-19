5b59dfbc04ac2ca6c93d242dfef1fff086161c68
joethyr <joethayr@gmail.com>
2021-11-19 07:06:10 -0700

    refactored code.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -17,7 +17,7 @@ class Hangman
   def game
     until @turns.zero?
       hide_display_word
-      print "\nEnter a letter for the hidden word:\n>"
+      print "\nEnter a letter to guess the hidden word:\n>"
       player_input = validate_player_input
       check_guess(player_input)
       check_player_won
@@ -28,7 +28,6 @@ class Hangman
   def validate_player_input
     input = gets.chomp.downcase
     if input.length != 1 || input.match(/[^a-z]/i)
-      puts input
       print "Invalid input. Please enter a letter from A-Z:\n>"
       validate_player_input
     elsif letters_used.include?(input)
@@ -52,7 +51,7 @@ class Hangman
   end
 
   def check_player_won
-    player_won if (display_word.map(&:downcase) - letters_used).empty?
+    player_won if (keyword.split('').map(&:downcase) - letters_used).empty?
   end
 
   def player_won
@@ -74,26 +73,17 @@ class Hangman
     play_again
   end
 
-  def display_word
-    keyword.split('')
-  end
-
   def new_game
     Hangman.new.game
   end
 
   def hide_display_word
-    display_word.map do |letter|
-      if letters_used.include?(letter.downcase)
-        print letter
-      else
-        print "-"
-      end
+    keyword.split('').map do |letter|
+      letters_used.include?(letter.downcase) ? (print letter) : (print '-')
     end
   end
-
 end
 
 play = Hangman.new
-p play.display_word
+# p play.display_word
 play.game
