b66c3851a2ae99bd02a52976668d4ecc9dc77fb5
joethyr <joethayr@gmail.com>
2021-11-18 20:03:55 -0700

    added method that checks if inputed letter is in hangman word.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 class Hangman
-  attr_reader :keyword, :letters_used
+  attr_reader :keyword, :letters_used, :turns
 
   def initialize
     @keyword = random_select_word
     @letters_used = []
+    @turns = 10
   end
 
   def random_select_word
@@ -13,25 +14,35 @@ class Hangman
   end
 
   def game
-    turns = 10
-    until turns.zero?
+    until @turns.zero?
       hide_display_word
       print "\nEnter a letter for the hidden word:\n>"
-      player_input
+      player_input = validate_player_input
+      check_guess(player_input)
     end
   end
 
-  def player_input
+  def validate_player_input
     input = gets.chomp.downcase
     if input.length != 1 || input.match(/[^a-z]/i)
       puts input
       print "Invalid input. Please enter a letter from A-Z:\n>"
-      player_input
+      validate_player_input
     elsif letters_used.include?(input)
       print "Input already used. Please enter a letter from A-Z:\n>"
-      player_input
+      validate_player_input
     else
       letters_used << input
+      input
+    end
+  end
+
+  def check_guess(player_input)
+    if !keyword.include?(player_input)
+      @turns -= 1
+      puts "incorrect letter!\n You have #{turns} turns left."
+    else
+      puts "correct!"
     end
   end
 
